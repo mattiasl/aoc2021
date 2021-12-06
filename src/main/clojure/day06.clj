73fd3c0773c2573c902a34af45c86d7551e0c920
Mattias Liljeström <mattias.liljestrom@evry.com>
2021-12-06 17:32:32 +0100

    day 6 - map -> vec

--- src/main/clojure/day06.clj
+++ src/main/clojure/day06.clj
@@ -4,24 +4,18 @@
 (def input (map #(Integer/parseInt %) (split (slurp "./src/main/clojure/day06.in") #",")))
 
 (defn input->initial-state [input]
-  (reduce merge
-          (reduce merge (for [x (range 9)] {x 0}))
+  (reduce (fn [a [k v]] (update a k + v))
+          (vec (repeat 9 0))
           (frequencies input)))
 
 (defn simulate-day [state]
-  (let [hatched (get state 0)
-        sixes-and-eights {6 (+ (get state 7) hatched), 8 hatched}]
-    (reduce (fn [a c]
-              (if (= c 8)
-                (merge a sixes-and-eights)
-                (assoc a c (get state (inc c)))))
-            state
-            (range 9))))
+  (let [hatched (first state)
+        new-state (conj (vec (rest state)) hatched)]
+    (update new-state 6 + hatched)))
 
 (defn solver [input days]
   (as-> (input->initial-state input) $
         (reduce (fn [state _] (simulate-day state)) $ (range days))
-        (vals $)
         (apply + $)))
 
 ;part 1
