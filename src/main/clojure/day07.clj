3344a94fdd2f511d0f4b40e7b0449519d3c49657
Mattias Liljeström <mattias.liljestrom@evry.com>
2021-12-07 17:09:49 +0100

    day 7

---
+++ src/main/clojure/day07.clj
@@ -0,0 +1,25 @@
+(ns day07
+  (:require [clojure.string :refer [split]]))
+
+(def input (map #(Integer/parseInt %) (split (slurp "./src/main/clojure/input/day07.in") #",")))
+
+(defn fuel-cost-part-1 [a b]
+  (Math/abs (- a b)))
+
+(defn sum-n [n]
+  (/ (* n (inc n)) 2))
+
+(defn fuel-cost-part-2 [a b]
+  (sum-n (fuel-cost-part-1 a b)))
+
+(defn solver [input fuel-cost-fn]
+(let [freq (frequencies input)]
+  (->> (range (apply min input) (inc (apply max input)))
+       (map (fn [x] (reduce (fn [a [k v]] (+ a (* v (fuel-cost-fn x k)))) 0 freq)))
+       (partition 2 1)
+       (some (fn [[a b]] (when (< a b) a))))))
+
+;part 1
+(time (solver input fuel-cost-part-1))
+;part 2
+(time (solver input fuel-cost-part-2))
